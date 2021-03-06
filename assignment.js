d499e83fa0fc8b902522888179d1abfe1cc5ce61
AngelaSophia <ayugisophiaangela@gmail.com>
2021-03-06 16:17:00 +0300

    assignment

---
+++ assignment.js
@@ -0,0 +1,12 @@
+function fruits(){
+    var fruit =["Banana","Orange","Mangoes"];
+    fruit.forEach((fruit)=>{
+        console.log(fruit)
+    
+});
+console.log(fruit[1]);
+console.log(fruit[0]);
+console.log(fruit[2]);
+
+}
+fruits()
