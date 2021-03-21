873b7ae1ba65120c1066427847383a574c8bd4b8
AngelaSophia <79142282+AngelaSophia@users.noreply.github.com>
2021-03-21 12:10:06 +0300

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,21 @@
+function getGrade ()  {
+   var marks =80;
+   var marks =75;
+   var marks =65;
+   var marks =53;
+   var marks =-40;
+   if (marks>=80)
+   {console.log("a");
+}else if(marks>=70)
+{console.log("b");
+}else if(marks>=60)
+{console.log("c");
+}else if(marks>=50)
+{console.log("d");
+}else if(marks>=0)
+{console.log("e");
+}else {
+   console.log("f");
+}
+}
+getGrade()
